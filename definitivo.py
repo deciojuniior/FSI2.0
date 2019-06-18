066278e7e33cb5e9ec8812eae0ac83b6d5e6d749
Murilo <murilofantozzi87@gmail.com>
2019-06-18 16:33:12 -0300

    first commit

---
+++ definitivo.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('star.jpg', 0)
+ret, thresh = cv2.threshold(img, 127, 255, 0)
+contours, hierarchy = cv2.findContours(thresh, 1, 2)
+
+cnt = contours[0]
+M = cv2.moments(cnt)
+print M
+
+area = cv2.contourArea(cnt)
+perimeter = cv2.arcLength(cnt,True)
+
+epsilon = 0.1*cv2.arcLength(cnt,True)
+approx = cv2.approxPolyDP(cnt,epsilon,True)
+
